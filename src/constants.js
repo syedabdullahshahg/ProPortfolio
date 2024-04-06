ca32cd42e3d381a43d8c9b01230205513ea585dd
syedabdullahshahg <102226538+syedabdullahshahg@users.noreply.github.com>
2024-04-06 06:04:55 +0100

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,18 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    PC: `This is my PC. I work mostly in JavaScript these days.
+      I've made a couple of projects in that language. I also like Java and Python. Anyway regardless of the language, I just like programming!`,
+    "ITDegree": `This is my IT degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+    "sofa_table": `That's my sofa. I like to relax here and watch YouTube." target="_blank">my channel</a>`,
+    tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+     <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
+    <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+    bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+    resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>`,
+    projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+    Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+    Library: `There are a lot of programming books on my shelves. There is even one in Urdu (I also speak Urdu btw).
+    I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+    exit: `If you want to exit my portfolio, just close the tab.`,
+  };
